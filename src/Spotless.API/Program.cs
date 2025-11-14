ce59bbf50f272532175254b9ce4f5f8aa6939a04
Mahmoud Atef <m.atef.business@gmail.com>
2025-11-14 02:04:30 +0200

    Restructure project to clean architecture

--- src/Spotless.API/Program.cs
+++ src/Spotless.API/Program.cs
@@ -1,3 +1,4 @@
+using Spotless.API.Extensions;
 
 namespace Spotless.API
 {
@@ -7,16 +8,21 @@ namespace Spotless.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
+            builder.Services.AddDatabaseConfiguration(builder.Configuration);
+            builder.Services.AddApplicationServices();
+            builder.Services.AddRepositories();
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -24,10 +30,7 @@ namespace Spotless.API
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
---
+++ src/Spotless.Domain/Interfaces/IRepositories/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Spotless.Domain.Entities;
+
+namespace Spotless.Domain.Interfaces.IRepositories
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
